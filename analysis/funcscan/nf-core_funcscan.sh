5c0035037915c2ba9b2bc9a5f3f933b307710439
carlosEcl06 <carlos.eugenio.cl@usp.br>
2026-05-08 12:05:14 -0300

    latest changes do params and configs for better computational resources usage

--- analysis/funcscan/nf-core_funcscan.sh
+++ analysis/funcscan/nf-core_funcscan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nextflow run /dados/felipe/Carlos_temporario/mining_on_preassembled_genomes/analysis/funcscan/pipeline/3_0_0/main.nf \
+NXF_VER=25.10.4 nextflow run /dados/felipe/Carlos_temporario/mining_on_preassembled_genomes/analysis/funcscan/pipeline/3_0_0/main.nf \
     -resume -profile apptainer -c custom.config -params-file params.yaml
